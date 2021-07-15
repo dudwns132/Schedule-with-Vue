b285b0891ccd18eddfb3e934b9ee2a87c49db2c1
dudwn <dudwns132@naver.com>
2021-07-15 18:41:34 +0900

    FullCalendar & Vue를 활용한 일정 추가 삭제 기능 구현

---
+++ src/components/Schedule.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <FullCalendar defaultView="dayGridMonth" :plugins="calendarPlugins" />
+    </div>
+</template>
+
+<script>
+
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import '@fullcalendar/core/main.css';
+import '@fullcalendar/daygrid/main.css';
+
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarPlugins: [ dayGridPlugin ]
+    }
+  }
+}
+
+</script>
